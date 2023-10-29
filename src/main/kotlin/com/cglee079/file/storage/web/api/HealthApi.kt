03379fd633aece0f47bcba1ffd42ca0860bf5ad1
cglee079 <cglee079@gmail.com>
2023-10-29 20:26:41 +0900

    feat : 용량 버그 수정

--- src/main/kotlin/com/cglee079/file/storage/web/api/HealthApi.kt
+++ src/main/kotlin/com/cglee079/file/storage/web/api/HealthApi.kt
@@ -1,10 +1,20 @@
 package com.cglee079.file.storage.web.api
 
+import org.springframework.boot.autoconfigure.web.servlet.MultipartProperties
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.annotation.PostConstruct
+import javax.servlet.MultipartConfigElement
 
 @RestController
-class HealthApi {
+class HealthApi(
+        val multipartConfigElement: MultipartConfigElement,
+){
+    @PostConstruct
+    fun init(){
+        println("multipartConfigElement.maxFileSize : ${multipartConfigElement.maxFileSize}")
+        println("multipartConfigElement.maxRequestSize : ${multipartConfigElement.maxRequestSize}")
+    }
     @GetMapping("/")
     fun home(): String {
         return "OK"
